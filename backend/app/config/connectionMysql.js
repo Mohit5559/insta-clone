53c9a9a28fb7d8004f0c04eb1a8b3aeeac11159a
MohitMishraFalna <mohitmishra.falna850@gmail.com>
2024-10-01 15:44:12 +0530

    Second commit

--- backend/app/config/connectionMysql.js
+++ backend/app/config/connectionMysql.js
@@ -3,10 +3,10 @@ const dbConnection = require('mysql');
 // connect db
 const con = dbConnection.createPool({
     max_user_connections:30,
-    connectionLimit : 100,
-    connectTimeout  : 60 * 60 * 1000,
-    acquireTimeout  : 60 * 60 * 1000,
-    timeout         : 60 * 60 * 1000,
+    // connectionLimit : 100,
+    // connectTimeout  : 60 * 60 * 1000,
+    // acquireTimeout  : 60 * 60 * 1000,
+    // timeout         : 60 * 60 * 1000,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
